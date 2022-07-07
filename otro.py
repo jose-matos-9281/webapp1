67be7860d1ef263a083f849fd9fd2761cd559e56
jose-matos-9281 <josegabrielmatos1@gmail.com>
2022-07-07 11:22:41 -0400

    jj

--- otro.py
+++ otro.py
@@ -1,8 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/',methods = ['GET'])
-def index():
-    return "Hola ta funcionando"
+
+from dash import Dash, html,dcc,Input, Output
+import plotly.express as px
+import pandas as pd
+import numpy as np
+import datetime as dt
+
+app = Dash(__name__)
+server = app.server
+app.layout = html.H1("hola mundo")
 
 if __name__ == '__main__':
     app.run()
